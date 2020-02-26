4e9418cde4dac519b0eea9916b7315b184048e32
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-26 21:08:20 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,7 @@ require 'pry'
 class Artist
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
+  extend Findable::ClassMethods
   include Paramable::InstanceMethods
  
   attr_accessor :name
